b235991889c83c12e5fdb430062bb4e1faaedc4e
166660614 <166660614@qq.com>
2019-01-15 16:47:50 +0800

    tr

--- app/Http/Controllers/Pay/AlipayController.php
+++ app/Http/Controllers/Pay/AlipayController.php
@@ -170,7 +170,7 @@ class AlipayController extends Controller
                 'plat_oid'=>$_POST['trade_no'],//支付宝订单号
                 'plat'=>1,//平台编号 1支付宝 2微信
             ];
-            OrderModel::where('order_id=>$order_id')->update($info);
+            OrderModel::where(['order_id'=>$order_id])->update($info);
         }
         //处理订单逻辑
         //$this->dealOrder($_POST);
