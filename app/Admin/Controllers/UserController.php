c5428dc0597a8066158bbb3732c03c4172b794ad
166660614 <166660614@qq.com>
2019-01-17 19:48:28 +0800

    用户管理

---
+++ app/Admin/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Admin\Controllers;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\Dashboard;
+use Encore\Admin\Layout\Column;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Layout\Row;
+use Encore\Admin\Grid;
+use Encore\Admin\Form;
+use App\Model\Info;
+class UserController extends Controller{
+    public function index(Content $content){
+        return $content
+            ->header('用户管理')
+            ->description('用户列表')
+            ->body($this->grid());
+    }
+    public function grid(){
+        $grid=new Grid(new Info());
+        $grid->id('用户ID');
+        $grid->name('用户名');
+        $grid->email('用户邮箱');
+        $grid->age('用户年龄');
+        $grid->reg_time('注册时间')->display(function ($time){
+            return date('Y-m-d H:i:s',$time);
+        });
+        return $grid;
+    }
+}
