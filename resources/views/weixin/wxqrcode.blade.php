3e12d577731e26500864ed0676ac384d02ebf4ea
166660614 <166660614@qq.com>
2019-02-27 11:46:39 +0800

    fsd

--- resources/views/weixin/wxqrcode.blade.php
+++ resources/views/weixin/wxqrcode.blade.php
@@ -1,160 +1,25 @@
-<?php
-
-namespace App\Http\Controllers\Pay;
-
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-
-use GuzzleHttp\Client;
-use App\Model\OrderModel;
-class WxPayController extends Controller
-{
-    public $weixin_unifiedorder_url = 'https://api.mch.weixin.qq.com/pay/unifiedorder';
-    public $weixin_notify_url = 'http://www.52self.cn/weixin/pay/notify';     //支付通知回调
-    public function unifiedOrder($order_id){
-        //请求参数
-        $orderInfo=OrderModel::where(['order_id'=>$order_id])->first();
-        $total_fee=$order_info['order_amount'];
-        $order_info=[
-            'appid'=>env('WEIXIN_APPID_0'),//微信支付绑定的服务号的APPID
-            'mch_id'=>env('WEIXIN_MCH_ID'), //商户ID
-            'nonce_str'=>str_random(16), //随机字符串
-            'sign_type'     => 'MD5',
-            'body'          => '筱川订单-'.mt_rand(1111,9999) . str_random(6),
-            'out_trade_no'  => $order_id,
-            'total_fee'     => $total_fee,
-            'spbill_create_ip'  => $_SERVER['REMOTE_ADDR'],//客户端ip
-            'notify_url'=>$this->weixin_notify_url,
-            'trade_type'    => 'NATIVE' //交易类型
-
-        ];
-        $this->values = [];
-        $this->values = $order_info;
-        $this->SetSign();
-        $xml=$this->toXml();//将数组转化为xml
-        $res = $this->postXmlCurl($xml, $this->weixin_unifiedorder_url, $useCert = false, $second = 30);
-        $data =  simplexml_load_string($res);
-        //生成二维码图片
-        $url=$data->code_url;
-        $arr=[
-            'url'=>$url,
-        ];
-        return view('weixin.wxqrcode',$arr);
-        //echo $url;exit;
-        /*$errorCorrectionLevel = 'H';//容错级别
-        $matrixPointSize = 7;//图片大小
-        $qr = rand(10000,99999).time().".png";
-        ob_clean();
-        $picture=QRcode::png($url, false, $errorCorrectionLevel, $matrixPointSize, 2);//2代表白边宽度
-        return view('weixin.wxqrcode')->with('picture',$picture);*/
-
-    }
-    public function SetSign()
-    {
-        $sign = $this->MakeSign();
-        $this->values['sign'] = $sign;
-        return $sign;
-    }
-    private function MakeSign()
-    {
-        //签名步骤一：按字典序排序参数
-        ksort($this->values);
-        $string = $this->ToUrlParams();
-        //签名步骤二：在string后加入KEY
-        $string = $string . "&key=".env('WEIXIN_MCH_KEY');
-        //签名步骤三：MD5加密
-        $string = md5($string);
-        //签名步骤四：所有字符转为大写
-        $result = strtoupper($string);
-        return $result;
-    }
-    /**
-     * 格式化参数格式化成url参数
-     */
-    protected function ToUrlParams()
-    {
-        $buff = "";
-        foreach ($this->values as $k => $v)
-        {
-            if($k != "sign" && $v != "" && !is_array($v)){
-                $buff .= $k . "=" . $v . "&";
-            }
-        }
-        $buff = trim($buff, "&");
-        return $buff;
-    }
-    protected function toXml(){
-        if(!is_array($this->values) || count($this->values)<=0){
-            die('数组数据异常');
-        }
-        $xml="<xml>";
-        foreach($this->values as $key=>$val){
-            if (is_numeric($val)){
-                $xml.="<".$key.">".$val."</".$key.">";
-            }else{
-                $xml.="<".$key."><![CDATA[".$val."]]></".$key.">";
-            }
-        }
-        $xml.="</xml>";
-        return $xml;
-    }
-    private  function postXmlCurl($xml, $url, $useCert = false, $second = 30)
-    {
-        $ch = curl_init();
-        //设置超时
-        curl_setopt($ch, CURLOPT_TIMEOUT, $second);
-        curl_setopt($ch,CURLOPT_URL, $url);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,TRUE);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,2);//严格校验
-        //设置header
-        curl_setopt($ch, CURLOPT_HEADER, FALSE);
-        //要求结果为字符串且输出到屏幕上
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        //		if($useCert == true){
-        //			//设置证书
-        //			//使用证书：cert 与 key 分别属于两个.pem文件
-        //			curl_setopt($ch,CURLOPT_SSLCERTTYPE,'PEM');
-        //			curl_setopt($ch,CURLOPT_SSLCERT, WxPayConfig::SSLCERT_PATH);
-        //			curl_setopt($ch,CURLOPT_SSLKEYTYPE,'PEM');
-        //			curl_setopt($ch,CURLOPT_SSLKEY, WxPayConfig::SSLKEY_PATH);
-        //		}
-        //post提交方式
-        curl_setopt($ch, CURLOPT_POST, TRUE);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
-        //运行curl
-        $data = curl_exec($ch);
-        //返回结果
-        if($data){
-            curl_close($ch);
-            return $data;
-        } else {
-            $error = curl_errno($ch);
-            curl_close($ch);
-            die("curl出错，错误码:$error");
-        }
-    }
-    public function notify(){
-        $data=file_get_contents("php://input");
-        //记录日志
-        $log_str = date('Y-m-d H:i:s') . "\n" . $data . "\n<<<<<<<";
-        file_put_contents('logs/wx_pay_notice.log',$log_str,FILE_APPEND);
-        $xml = simplexml_load_string($data);
-        if($xml->result_code=='SUCCESS' && $xml->return_code=='SUCCESS'){      //微信支付成功回调
-            //验证签名
-            $sign = true;
-
-            if($sign){       //签名验证成功
-                //TODO 逻辑处理  订单状态更新
-
-            }else{
-                //TODO 验签失败
-                echo '验签失败，IP: '.$_SERVER['REMOTE_ADDR'];
-                // TODO 记录日志
-            }
-
-        }
-
-        $response = '<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>';
-        echo $response;
-    }
-}
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>支付页面</title>
+</head>
+<body>
+<div id="qrcode" align="center"></div>
+<h2 align="center">扫&nbsp;&nbsp;码&nbsp;&nbsp;付&nbsp;&nbsp;款</h2>
+</body>
+</html>
+<script src="{{ URL::asset('js/qrcode/qrcode.min.js') }}"></script>
+<script type="text/javascript">
+    var qrcode = new QRCode(document.getElementById('qrcode'), {
+        text: "{{$url}}",
+        width: 256,
+        height: 256,
+        colorDark: '#000000',
+        colorLight: '#ffffff',
+        correctLevel: QRCode.CorrectLevel.H
+    });
+</script>
